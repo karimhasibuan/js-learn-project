6475d82fee9dd58bd88ce67278c663a227c0bee8
Karim Hasibuan <amfibicoding19@gmail.com>
2023-01-18 20:35:55 +0700

    feat: add js action

---
+++ feeling-today/index.js
@@ -0,0 +1,15 @@
+const feelInput = document.getElementById("feelInput");
+
+function feel() {
+  if (feelInput.value == "bad") {
+    alert("Hopefully tomorrow your day can be better. Spirit.!");
+  }
+
+  if (feelInput.value == "good") {
+    alert("Well Done. Keep Spirit!");
+  }
+
+  if (feelInput.value == "happy") {
+    alert("Wooo you are great. Come on keep spirit");
+  }
+}
